17c6d3222eb900689e24fcfdbc6d7575f68333de
martin023 <mainamartin023@gmail.com>
2022-07-26 22:47:04 +0300

    This commit adds javascript solution to playing banjo

---
+++ javascript-tests.js
@@ -0,0 +1,12 @@
+function areYouPlayingBanjo(name) {
+    if (name[0].toLowerCase() === 'r') {
+      return name + ' plays banjo';
+    } else {
+      console.log (name + ' does not play banjo');
+    }
+  }
+
+
+areYouPlayingBanjo("kevin") 
+
+// console.log("hello")
--- plays-banjo-string.py
+++ plays-banjo-string.py
@@ -15,3 +15,14 @@ def areYouPlayingBanjo(name):
         return name + ' does not play banjo'
 
 # in javascript
+
+# function areYouPlayingBanjo(name) {
+#     if (name[0].toLowerCase() === 'r') {
+#       return name + ' plays banjo';
+#     } else {
+#       console.log (name + ' does not play banjo');
+#     }
+#   }
+
+
+# areYouPlayingBanjo("kevin") 
