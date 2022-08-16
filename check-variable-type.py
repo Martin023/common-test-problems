fd4cd68452b4db8fd3a25aca685da72d936f4db6
Martin023 <mainamartin023@gmail.com>
2022-08-16 22:28:03 +0300

    Alternative method to check var type in python

--- check-variable-type.py
+++ check-variable-type.py
@@ -23,3 +23,13 @@ def filter_list(l):
             m.append(i)
             
     return m
+
+
+# type(variable) -is the second method we can use to check the type of var ie:
+def filter_list(l):
+    new_list =[]
+    for x in l:
+        if type(x) != str:
+            new_list.append(x)
+    return new_list
+             
