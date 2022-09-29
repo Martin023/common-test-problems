b17ec99e7160570b960926e4fd4a78dd07d5931e
Martin023 <mainamartin023@gmail.com>
2022-09-29 09:47:39 +0300

    match -case statement in python

---
+++ match-case.py
@@ -0,0 +1,22 @@
+##swich case statement in python uses match keyword for matching the cases instead of switch.
+## The match case statement was introduced in python 3.10
+## The match case statement is similar to switch case statement in other languages.
+## The match case statement is more readable and easy to understand.
+## The match case statement is more efficient than the switch case statement.
+## The match case statement is more flexible than the switch case statement.
+## The match case statement is more powerful than the switch case statement.
+## The match case statement is more concise than the switch case statement.
+## The match case statement is more elegant than the switch case statement.
+## The match case statement is more pythonic than the switch case statement.
+
+
+def switch_statement(num):
+    match num:
+        case 1:
+            print("One")
+        case 2:
+            print("Two")
+        case 3:
+            print("Three")
+        case _:
+            print("Unknown")
