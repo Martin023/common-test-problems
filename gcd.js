bb26ff0361f0a1b9a45188f61d239a0ac7fa76cf
Martin023 <mainamartin023#gmail.com>
2022-12-19 22:06:31 +0300

    python gcd

--- gcd.js
+++ gcd.js
@@ -7,3 +7,12 @@ function greatestCommonDivisor(num1,num2) {
     }
     }
 greatestCommonDivisor(4,16);
+
+
+//in python:
+//def greatestCommonDivisor (num1,num2):
+//  if num1%num2 == 0:
+//      print('The GCD is '+ num2)
+//  else:
+//      return greatestCommonDivisor(num2,num1%num2)
+
